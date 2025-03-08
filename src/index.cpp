50fa8165fb63e7fb60cfe34b3894d072f21bdf72
DataPirates30 <sumanthegreat30@gmail.com>
2025-03-08 00:33:31 -0600

    Creating nthd drray

--- src/index.cpp
+++ src/index.cpp
@@ -49,6 +49,9 @@ void stackVsHeap(){
 }
 int* create1Darray(int size){
     int* ptr = new int[size];
+    for(int i = 0; i< size;i++){
+        ptr[i] =  1;
+    }
     return ptr;
 }
 
@@ -65,7 +68,7 @@ void creatingAndShow1Darray()
     cin>>size;
     int* oned_arr = create1Darray(size);
     for(int i = 0; i< size;i++){
-        oned_arr[i] = i + 1;
+        oned_arr[i] =  1;
     }
     // Displaying the array
     // display1Darray(oned_arr,size);
@@ -76,14 +79,64 @@ void creatingAndShow1Darray()
     oned_arr= nullptr;
     cout<<oned_arr<<endl;
 }
+int** create2Darr(int size_of_row, int size_of_col){
+    int** ptr = new int*[size_of_row];
+    for(int i = 0;i<size_of_row;i++){
+        ptr[i] = create1Darray(size_of_col);
+    }
+    return ptr;
+}
 
-void creating2Darray(){
+void creatingandDisplay2Darray(){
+    // Creating a twod array using pointers
+    int size_of_col = 5;
+    int size_of_row = 3;
+    int** ptr = new int*[size_of_row];
+    for(int i = 0;i<3;i++){
+        ptr[i] = create1Darray(size_of_col);
+    }
+    for(int i = 0;i < size_of_row;i++){
+        for(int j = 0;j<size_of_col;j++){
+            cout<<ptr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
 }
 
-void creating3Darray(){
+void create3Darray(){
+    // I have succesfully created 1D and 2D array but it's turn of creating a 3D array
+    //3D array has 3 dimenesions
+    // Rows,cols, and depth
+    int depth_size = 2;
+    int row_size = 3;
+    int col_size = 5;
+
+    //I am going to construct a 3D array using pointers
+
+    int*** ptr =new int**[depth_size];
+    for(int i=0;i<depth_size;i++){
+        ptr[i] = create2Darr(row_size,col_size);
+    }
+
+    // Displaying 3D array
+    for(int i = 0; i<depth_size;i++){
+        for(int j = 0;j<row_size;j++){
+            cout<<"[";
+            for(int k= 0;k<col_size;k++){
+                cout<<ptr[i][j][k]<<" ";
+            }
+            cout<<"]";
+        }
+        cout<<endl;
+    }
 
 }
+
+void nthD_array(){
+    cout<<"Hello world"<<endl;
+}
+
 
 
 int main()  
@@ -91,7 +144,13 @@ int main()
     // returnPointer();
     // pointers();
     // stackVsHeap();
-    creatingAndShow1Darray();
+    // creatingAndShow1Darray();
+    // // creating2Darray();
+    // create3Darray();
+
+    nthD_array();
+    // Creatiung a nth dimension array is really hard
+
     return 0;
 }
 
